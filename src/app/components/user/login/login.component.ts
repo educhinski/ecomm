b2e85bdd9876be3be23be1d4bed6e8c554504848
educhinski <educhinski650@gmail.com>
2021-06-17 22:53:46 +0300

    Simple edits, routing bug fix and build error fix

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onClick(): void {
+    this.router.navigate(['../register'], { relativeTo: this.route });
   }
-
 }
