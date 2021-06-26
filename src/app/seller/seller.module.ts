4bd66536c0714e7645e500318fcdd98d9ed38fac
educhinski <educhinski650@gmail.com>
2021-06-27 01:28:52 +0300

    Change Home Component

--- src/app/seller/seller.module.ts
+++ src/app/seller/seller.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SellerHomeComponent } from './components/seller-home/seller-home.component';
+import { SellerRegisterComponent } from './components/seller-register/seller-register.component';
 
-const routes: Routes = [{ path: '', component: SellerHomeComponent }];
+const routes: Routes = [
+  { path: '', component: SellerHomeComponent },
+  { path: 'register', component: SellerRegisterComponent },
+];
 
 @NgModule({
-  declarations: [SellerHomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [SellerHomeComponent, SellerRegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
 })
 export class SellerModule {}
